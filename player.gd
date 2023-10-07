0f2fd235da54cd11199675b65061cd856f50a247
Matheus Alves Pereira <matttalves@gmail.com>
2023-10-07 15:07:41 -0300

    feat: mob and game logic

--- player.gd
+++ player.gd
@@ -1,9 +1,17 @@
 extends CharacterBody3D
 
+# Emitted when the player was hit by a mob.
+signal hit
+
 # How fast the player moves in meters per second.
 @export var speed = 14
 # The downward acceleation when in the air, in meters per second squared.
 @export var fall_acceleration = 75
+# Vertical impulse applied to the character upon jumping in meters per second.
+@export var jump_impulse = 20
+# Vertical impulse applied to the character upon bouncing over a mob in
+# meters per seconds.
+@export var bounce_impulse = 16
 
 var target_velocity = Vector3.ZERO
 
@@ -35,6 +43,33 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 	
+	
+	# Jumping
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+	
+	# Iterate through all collisions that occurred this frame
+	for index in range(get_slide_collision_count()):
+		# We get one of the collisions with the player
+		var collision = get_slide_collision(index)
+		var collider = collision.get_collider()
+		
+		# If the collision is with ground
+		if collider == null or not collider.is_in_group("mob"):
+			continue
+		# We check that we are hitting it from above.
+		if Vector3.UP.dot(collision.get_normal()) > 0.1:
+			# if so, we squash it and bounce.
+			collider.squash()
+			target_velocity.y = bounce_impulse
+	
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
+
+func die():
+	hit.emit()
+	queue_free()
+
+func _on_mob_detector_body_entered():
+	die()
